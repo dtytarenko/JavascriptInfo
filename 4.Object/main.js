55c2649ab05ed11a6d6999b419220fa20f8ec378
http404ntfnd <dmutro.tutarenko@gmail.com>
2019-08-30 21:01:43 +0300

    lesson 4.1 Object

---
+++ 4.Object/main.js
@@ -0,0 +1,55 @@
+// let user = {};
+// user.name = "John";
+// user.surname = "Smith";
+// console.log(user);
+// user.name = "Pete";
+// console.log(user);
+// delete user.name;
+// console.log(user);
+
+
+
+
+
+// function isEmpty(obj) {
+//     for (let key in obj) {
+//       return false;
+//     }
+//     return true;
+//   }
+
+// let schedule = {};
+// console.log(isEmpty(schedule));
+// schedule["8:30"] = "get up";
+// console.log(isEmpty(schedule));
+
+
+
+
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// }
+
+// let sum = 0;
+// for (let key in salaries){
+//     sum += salaries[key];
+// }
+
+// console.log(sum);
+
+
+
+
+// function multiplyNumeric(obj) {
+//     for (let key in obj){
+//         if (typeof obj[key] == 'number'){
+//             obj.key *= 2;
+//         }
+//     }
+// }
+
+
+
