9bb3cfeba2129bf44d5398ba731d1fc06e36bf04
http404ntfnd <dmutro.tutarenko@gmail.com>
2019-08-31 18:07:53 +0300

    end 4.1 oject methods. this

---
+++ 4.Methods_Object.This/main.js
@@ -0,0 +1,41 @@
+// let calculator = {
+//     read() {
+//         this.a = + prompt('a', 0);
+//         this.b = + prompt('b', 0);
+//     },
+
+//     sum() {
+//         return this.a + this.b;
+//     },
+
+//     mul(){
+//         return this.a * this.b;
+//     },
+// }
+
+// calculator.read();
+// console.log(calculator);
+// alert(calculator.sum());
+// alert(calculator.mul());
+
+
+
+
+
+let ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this;
+    },
+    down() {
+      this.step--;
+      return this;
+    },
+    showStep: function() { // показывает текущую ступеньку
+      alert( this.step );
+      return this;
+    }
+};
+
+console.log(ladder.up().up().down().showStep());
